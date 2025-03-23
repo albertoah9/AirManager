6ccc0a60f688b517d853dfed7970dc62ad2dba16
albertoah9 <soyalbertoalonso@gmail.com>
2025-03-23 12:22:30 +0100

    Errores y atributos de aeropuerto

---
+++ src/SistemaDeFacturacion.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+class SistemaDeFacturacion {
+    private ArrayList<Factura> facturas;
+
+    public SistemaDeFacturacion() {
+        this.facturas = new ArrayList<>(); 
+    }
+
+
+}
+//MIRAR BIEN
